5f04faaf2f702e88501e9d4196a7f4f2d0617c62
Verónica <bc.veros@gmail.com>
2022-08-25 18:40:26 -0500

    adding competencies in activities

--- src/activities/activitiesCreate.js
+++ src/activities/activitiesCreate.js
@@ -4,8 +4,10 @@ import {
     SimpleForm,
     Create,
     ReferenceInput,
-    SelectArrayInput,
-    required
+    required,
+    SelectInput,
+    ArrayInput,
+    SimpleFormIterator,
 
 } from 'react-admin';
 import { connect } from 'react-redux';
@@ -13,14 +15,28 @@ import { connect } from 'react-redux';
 const CreateTitle = () => {
     return <span>Crear Actividad</span>;
 };
+const choices = [
+    {label: "1", value: 1},
+    {label: "2", value: 2},
+    {label: "3", value: 3},
+    {label: "4", value: 4},
+    {label: "5", value: 5},
+];
+
+const maxPerPage = 999999999999999999999999999999999;
 
 export const ActivitiesCreateView = ({ permissions, ...props }) => (
     <Create {...props} title={<CreateTitle/>}>
         <SimpleForm redirect="list">
             <TextInput source="name" label="Actividad" validate={[required()]} />
-            <ReferenceInput source="competencies" label="Competencias" reference="competencies" sort={{ field: 'name', order: 'ASC' }}>
-                <SelectArrayInput optionText="name" validate={[required()]} />
-            </ReferenceInput>
+            <ArrayInput source="competencies">
+                <SimpleFormIterator>
+                    <ReferenceInput source="competencyId" label="Competencias" reference="competencies" sort={{ field: 'name', order: 'ASC' }} validate={[required()]} perPage={maxPerPage}>
+                        <SelectInput optionText="name" />
+                    </ReferenceInput>
+                    <SelectInput source="points" label="Puntos" choices={choices} optionText="label" optionValue="value" validate={[required()]} />  
+                </SimpleFormIterator>
+            </ArrayInput>
         </SimpleForm>
     </Create>
 );
