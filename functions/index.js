49dd5a7bdaab356424851fcd6a8021f4fc9861d3
Juan Lucena <jlucena@sic4change.org>
2025-05-12 17:27:37 +0200

    add lauraG to sheduledKpisFSE

--- functions/index.js
+++ functions/index.js
@@ -4539,7 +4539,7 @@ exports.sheduledKpisFSE = functions.pubsub.topic('sheduledKpisFSE').onPublish(as
         console.log('Informs Excel file URL:', informsUrl);
 
         return adminFirebase.firestore().collection('mail').add({
-            to: ['sic4change@gmail.com', 'aasencio@sic4change.org', 'asuarez@sic4change.org', 'bvillavicencio@sic4change.org'],
+            to: ['sic4change@gmail.com', 'aasencio@sic4change.org', 'asuarez@sic4change.org', 'bvillavicencio@sic4change.org', 'lgarcia@sic4change.org'],
             message: {
                 subject: `Informes Fondo Social Europeo (FSE).`,
                 html: `
